ada26429c840973ec03d366f7ebd0a95859f4f1d
joperopi <70579397+joperopi@users.noreply.github.com>
2024-08-12 15:20:14 -0300

    Add follow function

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("test");
+
+const container = document.getElementById('container');
+const follow = document.getElementById("follow");
+let ufoPosX = follow.offsetLeft;
+let ufoPosY = follow.offsetTop;
+
+container.addEventListener('mousemove', chase);
+
+console.log(follow.offsetLeft, follow.offsetTop);
+
+function chase(e){
+    if (ufoPosX > e.clientX) {
+        ufoPosX = ufoPosX - 1;
+    } else if (ufoPosX < e.clientX) {
+        ufoPosX = ufoPosX + 1;
+    }
+
+    if (ufoPosY > e.clientY) {
+        ufoPosY = ufoPosY - 1;
+    } else if (ufoPosY < e.clientY) {
+        ufoPosY = ufoPosY + 1;
+    }
+    console.log(ufoPosX, e.clientX)
+    follow.style.left = `${ufoPosX}px`;
+    follow.style.top = `${ufoPosY}px`;
+}
